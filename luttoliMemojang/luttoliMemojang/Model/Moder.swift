e01a45a8b3443cadba91b8aae25c57138dd0d978
gokmuli <77267009+gokmuli@users.noreply.github.com>
2021-10-23 22:36:55 +0900

    Memo class

---
+++ luttoliMemojang/luttoliMemojang/Model/Moder.swift
@@ -0,0 +1,25 @@
+//
+//  Moder.swift
+//  luttoliMemojang
+//
+//  Created by 김지훈 on 2021/10/23.
+//
+
+import Foundation
+
+//메모클레스 < 내용과 날짜 저장
+class Memo {
+    var content: String //메모 내용
+    var insertDate: Date //메모 생성한 날짜
+    
+    //클래스니까 속성을 초기화하는 속성자 생성
+    init(content: String) {
+        self.content = content
+        insertDate = Date()
+    }
+    
+    static var dummyMemoList = [
+        Memo(content: "Lorem Ipsum"),
+        Memo(content: "Subscribe + 👍 = ❤️")
+    ]
+}
